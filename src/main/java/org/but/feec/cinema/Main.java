63fcdc90ad328a42baa90012209fd29e80e011bd
240965 <240965@vutbr.cz>
2022-12-08 10:20:10 +0100

    setup done

--- src/main/java/org/but/feec/cinema/Main.java
+++ src/main/java/org/but/feec/cinema/Main.java
@@ -1,7 +1,14 @@
 package org.but.feec.cinema;
 
+import org.but.feec.cinema.config.DataSourceConfig;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args != null && args.length > 0) {
+            DataSourceConfig.initializeDataSource(args[0]);
+        } else {
+            DataSourceConfig.initializeDataSource(null);
+        }
+        App.main(args);
     }
 }
